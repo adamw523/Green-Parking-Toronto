9d1e752601f236611cd358ddbf98442b56b1421c
Adam Wisniewski <adamw@tbcn.ca>
2011-02-05 15:11:28 -0500

    adding licensing

--- src/ca/tbcn/greenp/CarparkActivity.java
+++ src/ca/tbcn/greenp/CarparkActivity.java
@@ -1,3 +1,25 @@
+/*
+Copyright (c) 2011 Adam Wisniewski (http://adamw523.com)
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+*/
+
 package ca.tbcn.greenp;
 
 import android.app.Activity;
@@ -12,7 +34,6 @@ import android.view.View.OnClickListener;
 import android.webkit.WebView;
 import android.widget.Button;
 import android.widget.ImageButton;
-import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -100,6 +121,9 @@ public class CarparkActivity extends Activity {
 		startActivity(intent);
 	}
 
+	/**
+	 * commenting out for now, not using WebView
+	 * 
 	private void loadWebsite() {
 		LinearLayout webWrapper = (LinearLayout) tvUrl.getParent(); 
 		webWrapper.removeAllViews();
@@ -110,6 +134,7 @@ public class CarparkActivity extends Activity {
 		webView.getSettings().setBuiltInZoomControls(true);
 		webView.loadUrl(carpark.getUrl());
 	}
+	 */
 	
 	private void sendToBrowser() {
 		String url = carpark.getUrl();
